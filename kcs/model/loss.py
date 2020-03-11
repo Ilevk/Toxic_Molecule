654b00cba878cc57026c9f499f02eeacaa962e70
Ilevk <kcs93023@gmail.com>
2020-03-11 11:55:54 +0900

    Edit loss.py
    Split F1_BCE_Loss

--- kcs/model/loss.py
+++ kcs/model/loss.py
@@ -3,6 +3,16 @@ from torch import nn
 from torch.nn import functional as F
 
 def F1_BCE_Loss(y_pred, y_true):
+
+    return F1_Loss(y_pred, y_true) + BCE_Loss(y_pred, y_true)
+
+def BCE_Loss(y_pred, y_true):
+    y_true = F.one_hot(y_true, 2).to(torch.float32)
+    y_pred = F.softmax(y_pred, dim=1)
+    
+    return F.binary_cross_entropy(y_pred, y_true)
+
+def F1_Loss(y_pred, y_true):
     assert y_pred.ndim == 2
     assert y_true.ndim == 1
     epsilon = 1e-7
@@ -20,5 +30,4 @@ def F1_BCE_Loss(y_pred, y_true):
     f1 = 2 * (precision * recall) / (precision + recall + epsilon)
     f1 = f1.clamp(min=epsilon, max=1 - epsilon)
 
-    bce = F.binary_cross_entropy(y_pred, y_true)
-    return 1 - f1.mean() + bce
+    return 1 - f1.mean()
